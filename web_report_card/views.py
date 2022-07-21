c556253978b27baa0d061fc27eb3caad3e139914
marnikvla <condor52@yandex.ru>
2022-07-21 15:09:13 +0300

    drag drop test

--- web_report_card/views.py
+++ web_report_card/views.py
@@ -1,10 +1,11 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 
 # Create your views here.
 from django.views.generic import TemplateView, ListView
-from django.views.generic.edit import FormMixin
+from django.views.generic.edit import FormMixin, FormView
 
 from web_report_card.forms import DocumentForm
 from web_report_card.models import Document
@@ -15,17 +16,14 @@ from web_report_card.models import Document
 
 
 
-class MyListView(ListView):
-
-    model = Document
+class MyListView(FormView):
+    form_class = DocumentForm
     template_name = 'web_report_card/main.html'
+    success_url = reverse_lazy('web_report_card:home')
 
-    def get(self, request, *args, **kwargs):
-        form = DocumentForm()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = DocumentForm(request.POST)
+    def form_valid(self, form):
+        Document.objects.create(**form.cleaned_data)
+        return redirect(self.get_success_url())
 
 
 class HomeView(TemplateView):
