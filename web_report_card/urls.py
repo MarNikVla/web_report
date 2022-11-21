744e882cce65feab62fde228e542a961aeeecd47
marnikvla <Nikolay12345it>
2022-11-21 21:42:38 +0300

    add errors handler.

--- web_report_card/urls.py
+++ web_report_card/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('test/', views.TestView.as_view(), name='test'),
     path('download/', views.FileDownloadView.as_view(), name='download'),
-    path('not_correct_file_upload/', views.NotCorrectFileUploadView.as_view(), name='not_correct_file_upload'),
+    path('not_correct_file_upload/<str:error>/', views.NotCorrectFileUploadView.as_view(), name='not_correct_file_upload'),
     path('upload/', views.file_upload),
 ]
--- web_report_card/views.py
+++ web_report_card/views.py
@@ -11,34 +11,48 @@ from django.views.generic import TemplateView
 from web_report_card.main import make_file_for_web_app
 
 IN_SESSION_FILE_KEY = 'uploaded_file'
+FILE_PATH = 'File_path'
+
+def unlink_file(file_path):
+    result_file_path_obj = pathlib.Path(file_path)
+    result_file_path_obj.unlink(missing_ok=True)
 
 class HomeView(TemplateView):
     template_name = 'web_report_card/main.html'
 
+
 class NotCorrectFileUploadView(TemplateView):
     template_name = 'web_report_card/errors.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['error'] = kwargs['error']
+        return data
+
 
 class TestView(TemplateView):
     template_name = 'web_report_card/test.html'
 
     def make_result(self, file_name):
-        try:
-            return make_file_for_web_app(file_name)
-        except:
-            raise Http404
+        result_file_path = make_file_for_web_app(file_name)
+        return result_file_path
 
     def get(self, request, *args, **kwargs):
         if IN_SESSION_FILE_KEY in request.session:
             file = request.session[IN_SESSION_FILE_KEY]
-            file_name = default_storage.save(file.name, file)
+            file_path = default_storage.save(file.name, file)
             del request.session[IN_SESSION_FILE_KEY]
-            result_file_path = self.make_result(file_name)
-            request.session['result_file_path'] = result_file_path
+            try:
+                result_file_path = self.make_result(file_path)
+                print(result_file_path)
+                request.session[FILE_PATH] = result_file_path
+            except Exception as e:
+                default_storage.delete(file_path)
+                return HttpResponseRedirect(reverse('web_report_card:not_correct_file_upload', kwargs={'error': e}))
             return super(TestView, self).get(request, *args, **kwargs)
         else:
-            # return HttpResponse('Загрузите корректный файл')
-            # return HttpResponseServerError("dff")
-            return HttpResponseRedirect(reverse('web_report_card:not_correct_file_upload'))
+            return HttpResponseRedirect(
+                reverse('web_report_card:not_correct_file_upload', kwargs={'error': 'Файл не загружен'}))
 
 
 def file_upload(request):
@@ -49,12 +63,11 @@ def file_upload(request):
 
 
 class FileDownloadView(View):
-
     content_type_value_xlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 
     def get(self, request, *args, **kwargs):
-        if 'result_file_path' in request.session:
-            file_path = request.session['result_file_path']
+        if FILE_PATH in request.session:
+            file_path = request.session[FILE_PATH]
             file = default_storage.open(file_path)
             result_file_path_obj = pathlib.Path(file.name)
             with file as fh:
@@ -63,8 +76,9 @@ class FileDownloadView(View):
                     content_type=self.content_type_value_xlsx
                 )
                 response['Content-Disposition'] = 'attachment; filename=' + result_file_path_obj.name
-            del request.session['result_file_path']
-            result_file_path_obj.unlink(missing_ok=True)
+            del request.session[FILE_PATH]
+            default_storage.delete(file_path)
+            # result_file_path_obj.unlink(missing_ok=True)
             return response
         else:
             raise Http404
